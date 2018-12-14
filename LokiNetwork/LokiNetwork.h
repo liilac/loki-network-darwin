56d261a2345075b6751921f29f95556592b5b3bb
Lilac <lilac@dissent.cloud>
2018-12-15 02:22:59 +1000

    Initial Commit

---
+++ LokiNetwork/LokiNetwork.h
@@ -0,0 +1,19 @@
+//
+//  LokiNetwork.h
+//  LokiNetwork
+//
+//  Created by Lilac on 15/12/18.
+//  Copyright © 2018 Loki Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LokiNetwork.
+FOUNDATION_EXPORT double LokiNetworkVersionNumber;
+
+//! Project version string for LokiNetwork.
+FOUNDATION_EXPORT const unsigned char LokiNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LokiNetwork/PublicHeader.h>
+
+
