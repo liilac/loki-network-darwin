43827aed47cd5faff5161458093ac51f48c884e0
Lilac <lilac@dissent.cloud>
2018-12-15 02:25:44 +1000

    Setup boilerplate framework project structure

--- LokiNetwork/LokiNetwork.h
+++ LokiNetwork/LokiNetwork.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char LokiNetworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LokiNetwork/PublicHeader.h>
 
-
+// import libllarp C API
+#import <llarp.h>
